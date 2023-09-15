a959d492db5a63de1da231d98974dbe200ceaab4
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2023-09-15 14:05:04 -0500

    Scripted auto-commit on change (2023-09-15 14:05:04) by gitwatch.sh

--- 01scheme/numbers.scm
+++ 01scheme/numbers.scm
@@ -1,4 +1,10 @@
 ;;; #t if all items in list are numbers
 ;;; #f otherwise
 (define list-of-numbers?
-  (lambda 
+  (lambda (lst)
+    (if (null? lst)
+	#t
+	(and (number? (car lst))
+	     (list-of-numbers? (cdr lst))))))
+
+	
