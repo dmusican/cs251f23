49ca78ed639e7cfdd75e745d669c454595c67cc1
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2023-09-18 13:57:54 -0500

    Scripted auto-commit on change (2023-09-18 13:57:54) by gitwatch.sh

---
+++ 02scheme/example1.scm
@@ -0,0 +1,12 @@
+;;; Currying (Haskell Curry)
+;;; Function of "multiple parameters" is instead
+;;; a function of one parameter than returns
+;;; another function
+
+;;; not curried
+(define mult-normal
+  (lambda (a b)
+    (* a b)))
+
+
+  
