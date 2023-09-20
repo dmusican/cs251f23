e76d45d55a353869871ede7d9bcc4271bef8a29d
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2023-09-20 14:27:39 -0500

    Scripted auto-commit on change (2023-09-20 14:27:39) by gitwatch.sh

--- 03scheme/example1.py
+++ 03scheme/example1.py
@@ -17,3 +17,11 @@ print(sorted(items,
     key=lambda animal_facts: animal_facts[1]))
 
 print(sorted(items, key=lambda val: randrange(1000)))
+
+# Make a new list where each item is one
+# more than in a previous list
+items = [9, 2, 1, 3, 5]
+new_items = items.copy()
+for i in range(len(new_items)):
+    new_items[i] = new_items[i] + 1
+print(new_items)
