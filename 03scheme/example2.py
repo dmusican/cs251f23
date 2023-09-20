2ae28aa8eed59895e8f3aa8fc403980f4ac9c79d
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2023-09-20 14:43:53 -0500

    Scripted auto-commit on change (2023-09-20 14:43:53) by gitwatch.sh

--- 03scheme/example2.py
+++ 03scheme/example2.py
@@ -12,3 +12,12 @@ items = [1, 2, 3]
 new_items = remove_last(items)
 print(new_items)
 print(items)
+
+# remove_first, with lots of side-effects
+count = 0
+def remove_first(lst):
+    global count
+    count = count + 1   # side-effect
+    lst.pop()      # removes first item from lst
+                    # ... so it's a side-effect
+    print("working")   # side-effect
