6b0d67370d536b11ed254b88edd2eefbe4456370
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2023-09-20 14:23:05 -0500

    Scripted auto-commit on change (2023-09-20 14:23:05) by gitwatch.sh

--- 03scheme/example1.py
+++ 03scheme/example1.py
@@ -1,2 +1,12 @@
 items = [('horse', 9), ('cow', 3), ('zebra', 0)]
 print(sorted(items))
+
+# key is a function of one parameter
+# that tells you what the value of the thing
+# should be for comparison purposes
+
+# dumb way
+def get_second_item(animal_facts):
+    return animal_facts[1]
+
+print(sorted(items, key=get_second_item)
