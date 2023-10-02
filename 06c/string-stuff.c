592b39c8dff1ac71d9759c137dc848c3b7e14548
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2023-10-02 14:16:25 -0500

    Scripted auto-commit on change (2023-10-02 14:16:25) by gitwatch.sh

--- 06c/string-stuff.c
+++ 06c/string-stuff.c
@@ -2,9 +2,11 @@
 
 
 int main() {
-    char pet[3];
+    /* char pet[3]; */
+    char *pet = malloc(3*sizeof(char));
     pet[0] = 'd';
     pet[1] = 'o';
     pet[2] = 'g';
     printf("%s\n", pet);
+
 }
