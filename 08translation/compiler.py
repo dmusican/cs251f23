219f2a3cc61d9f75a4d90d41fce186049b6cba17
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2023-10-09 14:28:06 -0500

    Scripted auto-commit on change (2023-10-09 14:28:06) by gitwatch.sh

--- 08translation/compiler.py
+++ 08translation/compiler.py
@@ -5,3 +5,9 @@ outfile = open('hello.bash', 'w')
 for line in inpfile:
     (command, number) = line.split()
     outfile.write('for i in {1..' + number + '}\n')
+    outfile.write('do\n')
+    outfile.write('    echo "' + command + '"\n')
+    outfile.write('done\n')
+
+inpfile.close()
+outfile.close()
