1debd15078e01446165268dcc15efd7884751b97
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2023-11-06 14:54:37 -0600

    Scripted auto-commit on change (2023-11-06 14:54:37) by gitwatch.sh

--- 12-types/InfExample.java
+++ 12-types/InfExample.java
@@ -2,4 +2,5 @@ public class InfExample {
     public static void main(String[] args) {
         // Scanner scanner = new Scanner();
         // String word1 = "hello";
+        // infers (figures out) that word1 is a String
         var word1 = "hello";
