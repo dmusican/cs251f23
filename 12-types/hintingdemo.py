edd9919c8bd545af78078432b0834e3958fc67b5
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2023-11-08 14:39:52 -0600

    Scripted auto-commit on change (2023-11-08 14:39:52) by gitwatch.sh

--- 12-types/hintingdemo.py
+++ 12-types/hintingdemo.py
@@ -5,7 +5,8 @@ def doit(x: Union[int, str]) -> int:
     y = int(x)
     #print(len(y))
     if type(x) == 'str':
-        print(len(x))
+        z = str(x)
+        print(len(z))
     if type(x) == 'int':
         return int(x)
     else:
