4a5015d877df0d16799af8b9d06679377b4f0fd4
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2023-11-08 14:41:58 -0600

    Scripted auto-commit on change (2023-11-08 14:41:58) by gitwatch.sh

--- 12-types/optionaldemo.py
+++ 12-types/optionaldemo.py
@@ -2,11 +2,11 @@ from typing import Optional
 
 
 def doit(x: Optional[int]) -> int:
-    return x+1
-    # if x is not None:
-    #     return x+1
-    # else:
-    #     return 0
+    # return x+1
+    if x is not None:
+        return x+1
+    else:
+        return 0
 
 
 def main():
