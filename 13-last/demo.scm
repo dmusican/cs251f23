b76d3c757241b753544d58c607e3d2700cc7abbd
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2023-11-08 14:51:36 -0600

    Scripted auto-commit on change (2023-11-08 14:51:36) by gitwatch.sh

--- 13-last/demo.scm
+++ 13-last/demo.scm
@@ -8,3 +8,10 @@
        (lambda (x)
          (+ x 1))))
   (one-time-function 3))
+
+(let ((fact
+       (lambda (x)
+         (if (equals? x 1)
+             1
+             (* x (fact (- x 1)))))))
+  (fact 5))
