021d574f6e8108f6077bf2bca4b7211a5b490b94
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2023-11-08 14:17:26 -0600

    Scripted auto-commit on change (2023-11-08 14:17:26) by gitwatch.sh

---
+++ 12-types/subtype.py
@@ -0,0 +1,25 @@
+class Dog:
+    def speak(self):
+        print("woof")
+
+
+class Cat:
+    def speak(self):
+        print("meow")
+
+
+class Giraffe:
+    pass
+
+
+def main():
+    spot = Dog()
+    fluffy = Cat()
+    longneck = Giraffe()
+
+    spot.speak()
+    fluffy.speak()
+    longneck.speak()
+
+
+main()
