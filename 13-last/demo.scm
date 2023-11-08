571364fc342f72cdcc6af23ce6a32a377c1cf52e
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2023-11-08 14:54:59 -0600

    Scripted auto-commit on change (2023-11-08 14:54:59) by gitwatch.sh

--- 13-last/demo.scm
+++ 13-last/demo.scm
@@ -9,9 +9,19 @@
          (+ x 1))))
   (one-time-function 3))
 
-(let ((fact
+;;; problem: lambda creates a closure that has an env
+;;; pointer that points to the parent frame, not the frame
+;;; that let just created
+;; (let ((fact
+;;        (lambda (x)
+;;          (if (equal? x 1)
+;;              1
+;;              (* x (fact (- x 1)))))))
+;;   (fact 5))
+
+(letrec ((fact
        (lambda (x)
          (if (equal? x 1)
              1
              (* x (fact (- x 1)))))))
-  5)
+  (fact 5))
