a2ccd618da7346400952fd342b66c159570829d0
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2023-11-15 14:10:20 -0600

    Scripted auto-commit on change (2023-11-15 14:10:20) by gitwatch.sh

--- 14-params/callby2.c
+++ 14-params/callby2.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-void foo(int x) {
-   x = 6;
+void foo(int *x) {
+   *x = 6;
 }
 
 int main() {
    int a = 5;
    printf("%i\n",a);
-   foo(a);
+   foo(&a);
    printf("%i\n",a);
 }
